c415accf4348c535da9a93e25458f97c5cd3a6ba
Fangyi Zhou <fangyi.zhou15@imperial.ac.uk>
2020-03-10 13:12:30 +0000

    Print time for auto updater

--- scripts/auto_update.py
+++ scripts/auto_update.py
@@ -1,6 +1,7 @@
 import asyncio
 import os
 import get_data
+from time import gmtime, strftime
 
 
 async def main():
@@ -8,6 +9,9 @@ async def main():
         with open(get_data.OUT_FILE) as f:
             old = f.read()
         get_data.main()
+        print(
+            "Fetched for updates, time:", strftime("%d %b %Y %H:%M:%S +0000", gmtime())
+        )
         with open(get_data.OUT_FILE) as f:
             new = f.read()
         if new != old:
